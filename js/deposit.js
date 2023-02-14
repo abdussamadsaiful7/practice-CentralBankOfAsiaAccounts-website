c7fa84669484fb0783c6d718e7ebd2f218c2ceb5
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-14 15:09:47 +0600

    add js function

---
+++ js/deposit.js
@@ -0,0 +1,25 @@
+document.getElementById('btn-deposit').addEventListener('click',function(){
+    const depositField = document.getElementById('deposit-field');
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+    
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+    depositTotalElement.innerText = newDepositAmount;
+
+    const currentDepositTotal =  previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = currentDepositTotal;
+    
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    const currentBalance = previousBalanceTotal + newDepositAmount;
+    balanceTotalElement.innerText  = currentBalance;
+
+
+
+
+    depositField.value = '';
+});
