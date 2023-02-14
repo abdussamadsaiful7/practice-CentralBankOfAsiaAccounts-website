ba83c061dd9dd4a141232c6d4df8682552641352
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-14 12:01:40 +0600

    added file

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ document.getElementById('btn-submit').addEventListener('click', function(){
     const password = passwordField.value;
     // danger: do not use for verify email & password
     
-    if(email === 'bank@gmail.com' && password === '12345'){
+    if(email === 'bank2@gmail.com' && password === '12345'){
         window.location.href ='http://127.0.0.1:5500/js/bank.html';
     }
     else{
